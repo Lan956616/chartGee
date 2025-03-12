6578837f74a8220f032c9993add381fe45e28aa5
Lan956616 <cd25924218@gmail.com>
2025-03-13 00:22:01 +0800

    feat: add reusable ColorSelect and ColorPicker components for SettingTab in edit/barchart

---
+++ src/components/settingtab/colorselect/colorselect.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useState } from "react";
+import styles from "./colorselect.module.css";
+import ColorPicker from "../colorpicker/colorpicker";
+type SelectProps = {
+  label: string;
+  color: string;
+  onChange: (newColor: string) => void;
+};
+const ColorSelect: React.FC<SelectProps> = ({ label, color, onChange }) => {
+  const [isPickerOpen, setIsPickerOpen] = useState<boolean>(false);
+  return (
+    <>
+      <div className={styles.selectWrapper}>
+        <p>{label}</p>
+        <div
+          className={styles.colorBox}
+          style={{ backgroundColor: `${color}` }}
+          onClick={() => {
+            setIsPickerOpen((prev) => !prev);
+          }}
+        ></div>
+        {isPickerOpen && (
+          <div className={styles.pickerWrapper}>
+            <ColorPicker
+              color={color}
+              onClick={(newColor) => {
+                onChange(newColor);
+                setIsPickerOpen(false);
+              }}
+            />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ColorSelect;
