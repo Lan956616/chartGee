91282bbc72fe4b7ae3af086d38226a0b77c52089
Lan956616 <cd25924218@gmail.com>
2025-03-13 22:14:33 +0800

    refactor: remove .wrapper from .main and adjust css styles

--- src/app/edit/barchart/page.tsx
+++ src/app/edit/barchart/page.tsx
@@ -4,26 +4,28 @@ import styles from "./style.module.css";
 import HeaderEditPage from "@/components/headereditpage/headereditpage";
 import Sidebar from "@/components/sidebar/sidebar";
 import ChartArea from "@/components/chartarea/chartarea";
-import { useState } from "react";
+import { useState, createContext } from "react";
 import {
   SampleBarChartdata,
   SampleBarChartoptions,
 } from "../../../../sampleChartData";
-
+export const ChartDataContext = createContext<any>(undefined);
 const BarChartEditPage: React.FC = () => {
   const [data, setData] = useState(SampleBarChartdata);
   const [options, setOptions] = useState(SampleBarChartoptions);
+
   return (
     <div className={styles.pageContainer}>
       <HeaderEditPage />
       <main className={styles.main}>
-        <div className={styles.wrapper}>
+        <ChartDataContext.Provider
+          value={{ data, setData, options, setOptions }}
+        >
           <DataArea />
           <ChartArea data={data} options={options} />
-        </div>
-
-        <Sidebar />
+        </ChartDataContext.Provider>
       </main>
+      <Sidebar />
     </div>
   );
 };
