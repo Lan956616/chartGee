5fff0cf7164197fe833cde9d6af1e40d41e76824
Lan956616 <cd25924218@gmail.com>
2025-03-23 15:00:47 +0800

    feat(dataTab): add editIcon with click handler and adjust css

--- src/components/dataarea/datatab/datatab.tsx
+++ src/components/dataarea/datatab/datatab.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useContext } from "react";
+import Image from "next/image";
+import { useContext, useRef } from "react";
 import type { ContextType } from "@/components/ChartDataProvider";
 import { ChartDataContext } from "@/components/ChartDataProvider";
 import { handleOptionChange } from "@/utils/updateOptions";
@@ -11,16 +12,30 @@ const DataTab: React.FC = () => {
   const { option, setOption } = useContext(
     ChartDataContext
   ) as unknown as ContextType;
+  const inputRef = useRef<HTMLInputElement | null>(null);
   return (
     <div className={styles.dataTabContainer}>
-      <div className={styles.chartTitleContainer}>
+      <div className={styles.TitleInputContainer}>
         <input
           type="text"
+          id="titleInput"
           value={option.plugins.title.text}
           onChange={(e) => {
             handleOptionChange(setOption, "plugins.title.text", e.target.value);
           }}
           placeholder="Add a title"
+          ref={inputRef}
+        />
+
+        <Image
+          src="/pencil.png"
+          alt="edit-icon"
+          width={20}
+          height={20}
+          className={styles.editIcon}
+          onClick={() => {
+            if (inputRef.current) inputRef.current.select();
+          }}
         />
       </div>
       <div className={styles.tableContainer}>
