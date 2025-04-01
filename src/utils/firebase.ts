bafd5ef109f00f9e605b4f20853528ebb7021171
Lan956616 <cd25924218@gmail.com>
2025-04-01 19:56:34 +0800

    feat: setup Firebase SDK and initialize app

---
+++ src/utils/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSUoMMfWk_JOZ8RFZuRGfn3-jJ6eZ1kXc",
+  authDomain: "chart-gee.firebaseapp.com",
+  projectId: "chart-gee",
+  storageBucket: "chart-gee.firebasestorage.app",
+  messagingSenderId: "23783322357",
+  appId: "1:23783322357:web:0995ac399d6890b7080bbc",
+  measurementId: "G-TDW0W104WF",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
