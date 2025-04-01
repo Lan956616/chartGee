60f74e83195f4c0a2ee384ff683141af7fc4881d
Lan956616 <cd25924218@gmail.com>
2025-04-02 02:20:31 +0800

    feat(signUpForm): connect Firebase Auth and add form validation

--- src/components/signup/signUpForm/signUpForm.tsx
+++ src/components/signup/signUpForm/signUpForm.tsx
@@ -1,15 +1,57 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useRouter } from "next/navigation";
 import styles from "./signUpForm.module.css";
 import Image from "next/image";
-
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/utils/firebase";
+import { FirebaseError } from "firebase/app";
 const SignUpForm: React.FC = () => {
-  const [step, setStep] = useState("two");
-  const [name, setName] = useState("");
+  const router = useRouter();
+  const emailInputRef = useRef<HTMLInputElement | null>(null);
+  const [step, setStep] = useState("one");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+    if (!email) {
+      setError("Email is required");
+      return;
+    }
+    const isValid = emailInputRef.current?.checkValidity();
+    if (!isValid) {
+      emailInputRef.current?.reportValidity();
+      return;
+    }
+    if (!password) {
+      setError("Password is required");
+      return;
+    }
+    if (password.length < 6) {
+      setError("Password must be at least 6 characters");
+      return;
+    }
+    try {
+      setIsLoading(true);
+      await createUserWithEmailAndPassword(auth, email, password);
+      router.push("/");
+    } catch (err: unknown) {
+      if (err instanceof FirebaseError) {
+        if (err.code === "auth/email-already-in-use") {
+          setError("This email is already registered");
+        } else {
+          setError("Signup failed Please try again");
+        }
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <form className={styles.signUpForm}>
+    <form className={styles.signUpForm} onSubmit={handleSubmit} noValidate>
       {step === "one" && (
         <div className={styles.stepOne}>
           <button
@@ -39,23 +81,16 @@ const SignUpForm: React.FC = () => {
               height={15}
               className={styles.arrow}
             />
-            Crate an account
+            Create an account
           </div>
           <input
-            type="text"
-            placeholder="Full Name"
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-          <input
-            type="text"
+            type="email"
             placeholder="Email"
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
             }}
+            ref={emailInputRef}
           />
           <p>Keep work and life separate. Use your work email.</p>
           <input
@@ -66,7 +101,23 @@ const SignUpForm: React.FC = () => {
               setPassword(e.target.value);
             }}
           />
-          <input type="submit" value="Register" />
+          {error && <p className={styles.error}>{error}</p>}
+          <button
+            type="submit"
+            className={styles.registerBTN}
+            disabled={isLoading}
+          >
+            {isLoading && (
+              <Image
+                src="/load.png"
+                alt="loading-icon"
+                width={20}
+                height={20}
+                className={styles.loadIcon}
+              />
+            )}
+            Register
+          </button>
         </div>
       )}
     </form>
