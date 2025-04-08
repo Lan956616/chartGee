9859339239665fb6300115147c9c1ac755141f6a
Lan956616 <cd25924218@gmail.com>
2025-04-08 19:44:52 +0800

    feat(dashboard): add chart selection UI and auth check logic

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useEffect } from "react";
+import { useAppSelector } from "@/lib/hooks";
+import { useRouter } from "next/navigation";
+import styles from "./style.module.css";
+import Header from "@/components/header/header";
+import Loading from "@/components/loading/loading";
+import GraphTypeCard from "@/components/dashboard/graphTypeCard/graphTypeCard";
+const DashBoardPage: React.FC = () => {
+  const router = useRouter();
+  const { currentUser: user, isAuthLoading } = useAppSelector((store) => {
+    return store.auth;
+  });
+  useEffect(() => {
+    if (isAuthLoading) {
+      return;
+    }
+    if (!isAuthLoading && !user) {
+      router.push("/login");
+    }
+  }, [user, router, isAuthLoading]);
+
+  if (isAuthLoading) {
+    return <Loading />;
+  }
+  return (
+    <div className={styles.pageContainer}>
+      <Header showCreateGraph={false} />
+      <p className={styles.title}>Create A Graph!</p>
+      <div className={styles.graphTypeList}>
+        <GraphTypeCard src="bar-chart" alt="Bar Graph Icon" label="Bar Graph" />
+        <GraphTypeCard src="pie-chart" alt="Pie Graph Icon" label="Pie Graph" />
+        <GraphTypeCard
+          src="line-chart"
+          alt="Line Graph Icon"
+          label="Line Graph"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DashBoardPage;
