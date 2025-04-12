2ed152b1c3f284ff24f8b615e651fcefc0caf42e
Lan956616 <cd25924218@gmail.com>
2025-04-12 12:48:55 +0800

    feat(headerProjectPage): add homepage Link to logo

--- src/components/projects/header/headerProjectPage.tsx
+++ src/components/projects/header/headerProjectPage.tsx
@@ -2,18 +2,22 @@
 import Container from "../../container/container";
 import styles from "./headerProjectPage.module.css";
 import Image from "next/image";
+import Link from "next/link";
 import UserButton from "./userButton/userButton";
 const HeaderProjectPage: React.FC = ({}) => {
   return (
     <header className={styles.header}>
       <Container>
         <div className={styles.headerWrapper}>
-          <Image
-            src="/chartGeeWithTitle.png"
-            alt="logo"
-            width={165}
-            height={45}
-          />
+          <Link href="/">
+            <Image
+              src="/chartGeeWithTitle.png"
+              alt="logo"
+              width={165}
+              height={45}
+            />
+          </Link>
+
           <UserButton />
         </div>
       </Container>
