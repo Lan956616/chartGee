6ed3a886b376a429fd4fcd64639d1ac40caab4c7
Lan956616 <cd25924218@gmail.com>
2025-04-12 09:41:10 +0800

    feat(sidebar): update nav links and add onClick to ListItem logout

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,5 +1,6 @@
 import styles from "./sidebar.module.css";
 import ListItem from "./listitem/listitem";
+import { handleSignOut } from "@/utils/signOutUser";
 const Sidebar: React.FC = () => {
   return (
     <nav className={styles.sidebar}>
@@ -7,13 +8,13 @@ const Sidebar: React.FC = () => {
         <ListItem href="/" src="/home.png" alt="home-icon">
           Home
         </ListItem>
-        <ListItem href="/" src="/newfile.png" alt="newfile-icon">
+        <ListItem href="/dashboard" src="/newfile.png" alt="newfile-icon">
           New File
         </ListItem>
-        <ListItem href="/" src="/graph.png" alt="graph-icon">
+        <ListItem href="/projects" src="/graph.png" alt="graph-icon">
           My Graph
         </ListItem>
-        <ListItem href="/" src="/logout.png" alt="logout-icon">
+        <ListItem src="/logout.png" alt="logout-icon" onClick={handleSignOut}>
           Log Out
         </ListItem>
       </ul>
