b0425a3bebfc8ae500324f460b98d5b79d9dedb8
Lan956616 <cd25924218@gmail.com>
2025-04-12 09:37:48 +0800

    feat(listItem):add onclick and lightMode  props

--- src/components/sidebar/listitem/listitem.tsx
+++ src/components/sidebar/listitem/listitem.tsx
@@ -1,19 +1,38 @@
 import styles from "./listitem.module.css";
 import Link from "next/link";
 import Image from "next/image";
-type ListItemProps = {
-  href: string;
-  src: string;
-  alt: string;
-  children: React.ReactNode;
-};
-const ListItem: React.FC<ListItemProps> = ({ href, src, alt, children }) => {
+type ListItemProps =
+  | {
+      href: string;
+      src: string;
+      alt: string;
+      children: React.ReactNode;
+      lightMode?: boolean;
+      onClick?: never;
+    }
+  | {
+      href?: never;
+      src: string;
+      alt: string;
+      children: React.ReactNode;
+      lightMode?: boolean;
+      onClick?: () => void;
+    };
+const ListItem: React.FC<ListItemProps> = (props) => {
+  const { src, alt, children } = props;
   return (
-    <li className={styles.listItem}>
-      <Link href={href} className={styles.itemWrapper}>
-        <Image src={src} alt={alt} width={30} height={30} />
-        <p>{children}</p>
-      </Link>
+    <li className={`${styles.listItem} ${props.lightMode && styles.lightMode}`}>
+      {props.href ? (
+        <Link href={props.href} className={styles.itemWrapper}>
+          <Image src={src} alt={alt} width={30} height={30} />
+          <p>{children}</p>
+        </Link>
+      ) : (
+        <button onClick={props.onClick} className={styles.itemWrapper}>
+          <Image src={src} alt={alt} width={30} height={30} />
+          <p>{children}</p>
+        </button>
+      )}
     </li>
   );
 };
