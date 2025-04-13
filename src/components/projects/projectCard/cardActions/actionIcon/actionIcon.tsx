d292712dfbdc46f961b9c7726738c6b66fea1780
Lan956616 <cd25924218@gmail.com>
2025-04-14 07:28:26 +0800

    refactor(ActionIcon): make ActionIcon a reusable component supporting link and button modes

--- src/components/projects/projectCard/cardActions/actionIcon/actionIcon.tsx
+++ src/components/projects/projectCard/cardActions/actionIcon/actionIcon.tsx
@@ -1,16 +1,47 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./actionIcon.module.css";
-type ActionIconProps = {
-  href: string;
+type BaseProps = {
   src: string;
   alt: string;
+  hint: string;
 };
-const ActionIcon: React.FC<ActionIconProps> = ({ href, src, alt }) => {
+
+type LinkProps = BaseProps & {
+  isLink: true;
+  href: string;
+  onClick?: never;
+};
+
+type ButtonProps = BaseProps & {
+  isLink: false;
+  onClick: () => void;
+  href?: never;
+};
+
+type ActionIconProps = LinkProps | ButtonProps;
+
+const ActionIcon: React.FC<ActionIconProps> = ({
+  href,
+  src,
+  alt,
+  isLink,
+  onClick,
+  hint,
+}) => {
   return (
-    <Link href={href} className={styles.actionIcon}>
-      <Image src={src} alt={alt} width={25} height={25} />
-    </Link>
+    <div className={styles.iconWrapper}>
+      {isLink ? (
+        <Link href={href} className={styles.actionIcon}>
+          <Image src={src} alt={alt} width={25} height={25} />
+        </Link>
+      ) : (
+        <button className={styles.actionIcon} onClick={onClick}>
+          <Image src={src} alt={alt} width={25} height={25} />
+        </button>
+      )}
+      <div className={styles.hint}>{hint}</div>
+    </div>
   );
 };
 
