ed0d31f73523045ab7a8524bfb05c0c559c41775
Lan956616 <cd25924218@gmail.com>
2025-04-14 10:41:53 +0800

    feat(userButton): add useClickWheelOutside hook

--- src/components/projects/header/userButton/userButton.tsx
+++ src/components/projects/header/userButton/userButton.tsx
@@ -1,21 +1,29 @@
 "use client";
 import Image from "next/image";
 import styles from "./userButton.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { handleSignOut } from "@/utils/signOutUser";
+import { useClickWheelOutside } from "@/hooks/useClickWheelOutside";
 const UserButton: React.FC = () => {
   const [isClicked, setIsClicked] = useState(false);
+  const iconRef = useRef<HTMLDivElement>(null);
+  const popupRef = useRef<HTMLButtonElement>(null);
+  useClickWheelOutside(popupRef, iconRef, isClicked, () => {
+    setIsClicked(false);
+  });
   return (
     <div
       className={`${styles.iconWrapper} ${isClicked && styles.clicked}`}
       onClick={() => {
         setIsClicked((prev) => !prev);
       }}
+      ref={iconRef}
     >
       <Image src="/user.png" alt="user icon" width={20} height={20} />
       <button
         className={`${styles.logOutButton} ${isClicked && styles.active}`}
         onClick={handleSignOut}
+        ref={popupRef}
       >
         Log Out
       </button>
