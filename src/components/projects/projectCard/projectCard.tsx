f1c990fe2b1c64ee6272a44d3a018338aff4b57d
Lan956616 <cd25924218@gmail.com>
2025-04-15 11:41:43 +0800

    feat: complete ProjectCard component for projects page thumbnail pending

---
+++ src/components/projects/projectCard/projectCard.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { useState } from "react";
+import styles from "./projectCard.module.css";
+import Image from "next/image";
+import CardActions from "./cardActions/cardActions";
+import { getChartTypeIcon } from "@/utils/getChartTypeIcon";
+import SharePopUp from "./sharePopUp/sharePopUp";
+import DeletePopUp from "./deletePopUp/deletePopUp";
+type ProjectCardProps = {
+  chartType: "pie" | "line" | "bar";
+  updatedAt: string;
+  title: string;
+  id: string;
+};
+
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  chartType,
+  updatedAt,
+  title,
+  id,
+}) => {
+  const [showSharePopup, setShowSharePopup] = useState(false);
+  const [showDeletePopup, setShowDeletePopup] = useState(false);
+  return (
+    <div className={styles.projectCardContainer}>
+      <div className={styles.thumbnailWrapper}></div>
+      <CardActions
+        projectID={id}
+        setShowDeletePopup={setShowDeletePopup}
+        setShowSharePopup={setShowSharePopup}
+      />
+      <div className={styles.cardInfo}>
+        <Image
+          src={getChartTypeIcon(chartType)}
+          alt={`${chartType} chart icon`}
+          width={25}
+          height={25}
+        />
+        <div className={styles.description}>
+          <p className={styles.title}>{title || "Untitled Graph"}</p>
+          <p className={styles.updatedTime}>{`Updated ${updatedAt}`}</p>
+        </div>
+      </div>
+      {showSharePopup && (
+        <SharePopUp projectID={id} setShowSharePopup={setShowSharePopup} />
+      )}
+      {showDeletePopup && (
+        <DeletePopUp projectID={id} setShowDeletePopup={setShowDeletePopup} />
+      )}
+    </div>
+  );
+};
+
+export default ProjectCard;
