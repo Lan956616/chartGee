8c92699d3b39def2e6723402118252de2bdeb56a
Lan956616 <cd25924218@gmail.com>
2025-04-16 07:09:33 +0800

    feat: also delete public project when deleting user project

--- src/utils/handleDeleteProject.ts
+++ src/utils/handleDeleteProject.ts
@@ -2,5 +2,7 @@ import { db } from "./firebase";
 import { deleteDoc, doc } from "firebase/firestore";
 export const handleDeleteProject = async (uid: string, projectID: string) => {
   const projectRef = doc(db, "users", uid, "projects", projectID);
+  const publicProjectRef = doc(db, "publicProjects", projectID);
   await deleteDoc(projectRef);
+  await deleteDoc(publicProjectRef);
 };
