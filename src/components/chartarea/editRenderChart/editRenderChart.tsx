11fd946ed3b7f8f2eec2a153ff791660ceb0728b
Lan956616 <cd25924218@gmail.com>
2025-04-17 18:02:37 +0800

    feat(editRenderChart): extract chart render logic from chartArea component

---
+++ src/components/chartarea/editRenderChart/editRenderChart.tsx
@@ -0,0 +1,71 @@
+import styles from "./editRenderChart.module.css";
+import { ProjectDataType } from "@/utils/sampleChartData/projectDataType";
+import { backgroundColorPlugin } from "@/utils/backgroundColorPlugin";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  Title,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement,
+} from "chart.js";
+import { getCleanData } from "@/utils/getCleanData";
+import { getCleanPieData } from "@/utils/getCleanPieData";
+import { Pie, Bar, Line } from "react-chartjs-2";
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  Title,
+  ChartDataLabels,
+  backgroundColorPlugin,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement
+);
+
+const EditRenderChart: React.FC<ProjectDataType> = ({
+  chartType,
+  data,
+  option,
+}) => {
+  if (chartType === "bar") {
+    return (
+      <Bar
+        key={option.aspectRatio}
+        data={getCleanData(data)}
+        options={option}
+        className={styles.chart}
+      />
+    );
+  }
+  if (chartType === "line") {
+    return (
+      <Line
+        key={option.aspectRatio}
+        data={getCleanData(data)}
+        options={option}
+        className={styles.chart}
+      />
+    );
+  }
+  if (chartType === "pie") {
+    return (
+      <Pie
+        key={option.aspectRatio}
+        data={getCleanPieData(data)}
+        options={option}
+        className={styles.chart}
+      />
+    );
+  }
+};
+
+export default EditRenderChart;
