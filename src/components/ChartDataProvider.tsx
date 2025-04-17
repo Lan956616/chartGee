92e5007fae92a66a8b4f4283d2c73a00e7f21cb5
Lan956616 <cd25924218@gmail.com>
2025-04-17 14:21:46 +0800

    refactor(ChartDataProvider): update  to receive external chart state

--- src/components/ChartDataProvider.tsx
+++ src/components/ChartDataProvider.tsx
@@ -1,80 +1,26 @@
 "use client";
-import { createContext, useState } from "react";
-import {
-  SampleBarChartdata,
-  SampleBarChartoptions,
-} from "../utils/sampleChartData/barChart";
-import type {
-  SampleBarChartData,
-  SampleBarChartOptions,
-} from "../utils/sampleChartData/barChartDataType";
-import type { Dispatch, SetStateAction } from "react";
-import {
-  samplePieChartData,
-  samplePieChartOption,
-} from "@/utils/sampleChartData/pieChart";
-import type {
-  SamplePieChartData,
-  SamplePieChartOption,
-} from "@/utils/sampleChartData/pieChartDataType";
-import {
-  sampleLineChartData,
-  sampleLineChartOption,
-} from "@/utils/sampleChartData/lineChart";
-import {
-  SampleLineChartData,
-  SampleLineChartOption,
-} from "@/utils/sampleChartData/lineChartDataType";
+import { createContext } from "react";
+import { ProjectDataType } from "@/utils/sampleChartData/projectDataType";
+import { Dispatch } from "react";
+import type { SetStateAction } from "react";
 
-export type ContextType = {
-  data: SampleBarChartData;
-  setData: Dispatch<SetStateAction<SampleBarChartData>>;
-  option: SampleBarChartOptions;
-  setOption: Dispatch<SetStateAction<SampleBarChartOptions>>;
-  pieData: SamplePieChartData;
-  setPieData: Dispatch<SetStateAction<SamplePieChartData>>;
-  pieOption: SamplePieChartOption;
-  setPieOption: Dispatch<SetStateAction<SamplePieChartOption>>;
-  lineData: SampleLineChartData;
-  setLineData: Dispatch<SetStateAction<SampleLineChartData>>;
-  lineOption: SampleLineChartOption;
-  setLineOption: Dispatch<SetStateAction<SampleLineChartOption>>;
-  unit: string;
-  setUnit: Dispatch<SetStateAction<string>>;
+type ChartDataContextType = {
+  currentData: ProjectDataType | null;
+  setCurrentData: Dispatch<SetStateAction<ProjectDataType | null>>;
 };
-export const ChartDataContext = createContext<ContextType | undefined>(
-  undefined
+export const ChartDataContext = createContext<ChartDataContextType | null>(
+  null
 );
-
-const ChartDataProvider: React.FC<{ children: React.ReactNode }> = ({
+type ChartDataProviderProps = {
+  children: React.ReactNode;
+  value: ChartDataContextType;
+};
+const ChartDataProvider: React.FC<ChartDataProviderProps> = ({
   children,
+  value,
 }) => {
-  const [data, setData] = useState(SampleBarChartdata);
-  const [option, setOption] = useState(SampleBarChartoptions);
-  const [pieData, setPieData] = useState(samplePieChartData);
-  const [pieOption, setPieOption] = useState(samplePieChartOption);
-  const [lineData, setLineData] = useState(sampleLineChartData);
-  const [lineOption, setLineOption] = useState(sampleLineChartOption);
-  const [unit, setUnit] = useState("kg");
   return (
-    <ChartDataContext.Provider
-      value={{
-        data,
-        setData,
-        option,
-        setOption,
-        pieData,
-        setPieData,
-        pieOption,
-        setPieOption,
-        lineData,
-        setLineData,
-        lineOption,
-        setLineOption,
-        unit,
-        setUnit,
-      }}
-    >
+    <ChartDataContext.Provider value={value}>
       {children}
     </ChartDataContext.Provider>
   );
