88557d531553ee201363de6fe2ec38c3e95cbdb0
Lan956616 <cd25924218@gmail.com>
2025-04-18 12:16:08 +0800

    refactor(dataTab): update option from context and replace onChange logic

--- src/components/dataarea/datatab/datatab.tsx
+++ src/components/dataarea/datatab/datatab.tsx
@@ -1,57 +1,31 @@
 "use client";
 import Image from "next/image";
 import { useContext, useRef } from "react";
-import type { ContextType } from "@/components/ChartDataProvider";
 import { ChartDataContext } from "@/components/ChartDataProvider";
-import { handleOptionChange } from "@/utils/updateOptions";
 import styles from "./datatab.module.css";
 import DataTableHeader from "./datatableheader/datatableheader";
 import DataTableBody from "./datatablebody/datatablebody";
 import TableControlButton from "./tablecontrolbutton/tablecontrolbutton";
-
 import PieTableBody from "@/components/pieChart/PieTableBody/PieTableBody";
-type DataTabProps = { chartType: "bar" | "pie" | "line" };
-const DataTab: React.FC<DataTabProps> = ({ chartType }) => {
-  const {
-    option,
-    setOption,
-    pieOption,
-    setPieOption,
-    lineOption,
-    setLineOption,
-  } = useContext(ChartDataContext) as unknown as ContextType;
-  const OptionName =
-    chartType === "bar" ? option : chartType === "pie" ? pieOption : lineOption;
-  const setOptionName =
-    chartType === "bar"
-      ? setOption
-      : chartType === "pie"
-      ? setPieOption
-      : setLineOption;
+import { updateOption } from "@/utils/updateOptions";
+const DataTab: React.FC = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
-  const TableHeaderComponent =
-    chartType === "bar" || chartType === "line" ? (
-      <DataTableHeader chartType={chartType} />
-    ) : null;
-  const TableBodyComponent =
-    chartType === "bar" || chartType === "line" ? (
-      <DataTableBody chartType={chartType} />
-    ) : (
-      <PieTableBody />
-    );
+  const context = useContext(ChartDataContext);
+  if (!context?.currentData) return;
+  const { setCurrentData } = context;
+  const { chartType, option } = context.currentData;
+  const isPieChart = chartType === "pie";
+  const TableHeaderComponent = isPieChart ? null : <DataTableHeader />;
+  const TableBodyComponent = isPieChart ? <PieTableBody /> : <DataTableBody />;
   return (
     <div className={styles.dataTabContainer}>
       <div className={styles.TitleInputContainer}>
         <input
           type="text"
           id="titleInput"
-          value={OptionName.plugins.title.text}
+          value={option.plugins.title.text}
           onChange={(e) => {
-            handleOptionChange(
-              setOptionName,
-              "plugins.title.text",
-              e.target.value
-            );
+            updateOption(setCurrentData, "plugins.title.text", e.target.value);
           }}
           placeholder="Add a title"
           ref={inputRef}
@@ -74,7 +48,7 @@ const DataTab: React.FC<DataTabProps> = ({ chartType }) => {
           {TableBodyComponent}
         </table>
       </div>
-      <TableControlButton chartType={chartType} />
+      <TableControlButton />
     </div>
   );
 };
