53fd97cb7513f64e941270335dca7af2fefc7dbd
Lan956616 <cd25924218@gmail.com>
2025-04-19 10:13:18 +0800

    feat(unitSetting): remove props and get graph info from context and replace onchange function

--- src/components/settingtab/axespanel/unitsetting/unitsetting.tsx
+++ src/components/settingtab/axespanel/unitsetting/unitsetting.tsx
@@ -2,73 +2,63 @@ import Slider from "../../slider/slider";
 import SelectDropDown from "../../selectdropdown/selectdropdown";
 import ColorSelect from "../../colorselect/colorselect";
 import { ChartDataContext } from "@/components/ChartDataProvider";
-import type { ContextType } from "@/components/ChartDataProvider";
 import { useContext } from "react";
-import { handleOptionChange } from "@/utils/updateOptions";
 import TextInput from "../../textinput/textinput";
-type UnitSettingProps = { chartType: "bar" | "line" };
-const UnitSetting: React.FC<UnitSettingProps> = ({ chartType }) => {
-  const { option, setOption, lineOption, setLineOption } = useContext(
-    ChartDataContext
-  ) as unknown as ContextType;
-  const Option = chartType === "bar" ? option : lineOption;
-  const SetOption = chartType === "bar" ? setOption : setLineOption;
+import { updateMultipleOptions } from "@/utils/updateMutipleOptions";
+const UnitSetting: React.FC = () => {
+  const context = useContext(ChartDataContext);
+  if (!context?.currentData) return;
+  const { setCurrentData } = context;
+  const { option, chartType } = context.currentData;
+  if (chartType === "pie") return;
   return (
     <div>
       <TextInput
         label="Unit"
-        value={Option.scales.x.title.text}
+        value={option.scales.x.title.text}
         onChange={(newUnit) => {
-          handleOptionChange(SetOption, "scales.x.title.text", newUnit);
-          handleOptionChange(SetOption, "scales.y.title.text", newUnit);
+          updateMultipleOptions(setCurrentData, [
+            ["scales.x.title.text", newUnit],
+            ["scales.y.title.text", newUnit],
+          ]);
         }}
       />
       <Slider
         label="Font Size"
         min={6}
         max={40}
-        value={Option.scales.x.title.font.size}
+        value={option.scales.x.title.font.size}
         Unit="px"
         onChange={(newFontSize) => {
-          handleOptionChange(
-            SetOption,
-            "scales.x.title.font.size",
-            newFontSize
-          );
-          handleOptionChange(
-            SetOption,
-            "scales.y.title.font.size",
-            newFontSize
-          );
+          updateMultipleOptions(setCurrentData, [
+            ["scales.x.title.font.size", newFontSize],
+            ["scales.y.title.font.size", newFontSize],
+          ]);
         }}
       />
       <SelectDropDown
         label="Font Weight"
-        value={Option.scales.x.title.font.weight}
+        value={option.scales.x.title.font.weight}
         width={100}
         options={[
           { value: "normal", label: "Normal" },
           { value: "bold", label: "Bold" },
         ]}
         onChange={(newFontWeight) => {
-          handleOptionChange(
-            SetOption,
-            "scales.x.title.font.weight",
-            newFontWeight
-          );
-          handleOptionChange(
-            SetOption,
-            "scales.y.title.font.weight",
-            newFontWeight
-          );
+          updateMultipleOptions(setCurrentData, [
+            ["scales.x.title.font.weight", newFontWeight],
+            ["scales.y.title.font.weight", newFontWeight],
+          ]);
         }}
       />
       <ColorSelect
         label="Font Color"
-        color={Option.scales.x.title.color}
+        color={option.scales.x.title.color}
         onChange={(newColor) => {
-          handleOptionChange(SetOption, "scales.x.title.color", newColor);
-          handleOptionChange(SetOption, "scales.y.title.color", newColor);
+          updateMultipleOptions(setCurrentData, [
+            ["scales.x.title.color", newColor],
+            ["scales.y.title.color", newColor],
+          ]);
         }}
       />
     </div>
