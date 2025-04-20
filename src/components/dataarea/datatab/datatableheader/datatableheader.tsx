d1e66339323911433376aabe4e4142796e91bdc2
Lan956616 <cd25924218@gmail.com>
2025-04-20 10:45:35 +0800

    refactor(dataTableHeader):get graph info from context and replace onchange func

--- src/components/dataarea/datatab/datatableheader/datatableheader.tsx
+++ src/components/dataarea/datatab/datatableheader/datatableheader.tsx
@@ -1,31 +1,19 @@
 "use client";
 import { useContext } from "react";
-import type { ContextType } from "@/components/ChartDataProvider";
 import { ChartDataContext } from "@/components/ChartDataProvider";
 import styles from "./datatableheader.module.css";
 import Image from "next/image";
-import { handleOptionChange } from "@/utils/updateOptions";
 import { handleLabelChange } from "@/utils/updateData";
 import { handleInputKeyDown } from "@/utils/handleInputKeyDown";
 import getAxisInfo from "@/utils/getAxisInfo";
-type DataTableHeaderProps = { chartType: "bar" | "line" };
-const DataTableHeader: React.FC<DataTableHeaderProps> = ({ chartType }) => {
-  const {
-    data,
-    setData,
-    setOption,
-    option,
-    lineData,
-    setLineData,
-    lineOption,
-    setLineOption,
-  } = useContext(ChartDataContext) as unknown as ContextType;
-  const Option = chartType === "bar" ? option : lineOption;
-  const SetOption = chartType === "bar" ? setOption : setLineOption;
-  const Data = chartType === "bar" ? data : lineData;
-  const SetData = chartType === "bar" ? setData : setLineData;
-  const { valueAxis, labelAxis } = getAxisInfo(Option.indexAxis);
-  const currentDisplay = Option.scales[valueAxis].title.display;
+import { updateMultipleOptions } from "@/utils/updateMutipleOptions";
+const DataTableHeader: React.FC = () => {
+  const context = useContext(ChartDataContext);
+  if (!context?.currentData) return;
+  const { setCurrentData } = context;
+  const { chartType, data, option } = context.currentData;
+  if (chartType === "pie") return;
+
   return (
     <thead className={styles.thead}>
       <tr className={styles.tableRow}>
@@ -44,23 +32,19 @@ const DataTableHeader: React.FC<DataTableHeaderProps> = ({ chartType }) => {
             height={30}
             className={styles.axisIconHover}
             onClick={() => {
-              handleOptionChange(SetOption, "indexAxis", valueAxis);
-              handleOptionChange(
-                SetOption,
-                `scales.${labelAxis}.title.display`,
-                currentDisplay
-              );
-              handleOptionChange(
-                SetOption,
-                `scales.${valueAxis}.title.display`,
-                false
-              );
+              const { valueAxis, labelAxis } = getAxisInfo(option.indexAxis);
+              const currentDisplay = option.scales[valueAxis].title.display;
+              updateMultipleOptions(setCurrentData, [
+                ["indexAxis", valueAxis],
+                [`scales[${valueAxis}].title.display`, false],
+                [`scales[${labelAxis}].title.display`, currentDisplay],
+              ]);
             }}
           />
         </th>
         <th className={styles.cell}></th>
-        {Data.labels.map((label, index) => {
-          const hasData = Data.datasets.some(
+        {data.labels.map((label, index) => {
+          const hasData = data.datasets.some(
             (dataset) =>
               dataset.data[index] !== "" && dataset.data[index] !== null
           );
@@ -77,7 +61,7 @@ const DataTableHeader: React.FC<DataTableHeaderProps> = ({ chartType }) => {
                 value={label}
                 placeholder={`Cate${index + 1}`}
                 onChange={(e) => {
-                  handleLabelChange(SetData, e.target.value, index);
+                  handleLabelChange(setCurrentData, e.target.value, index);
                 }}
                 onKeyDown={handleInputKeyDown}
                 onFocus={(e) => {
