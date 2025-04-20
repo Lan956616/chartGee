a013fd2d71dbee1cbf0fe4eac381742ccfa460f8
Lan956616 <cd25924218@gmail.com>
2025-04-20 21:27:56 +0800

    refactor(types): extract RenderDataType, ShareDataType, and StripDataType from ProjectDataType

--- src/utils/sampleChartData/projectDataType.ts
+++ src/utils/sampleChartData/projectDataType.ts
@@ -31,19 +31,62 @@ export type ProjectDataType =
       isPublic: boolean;
     };
 
-// export type ChartDataContextType =
-//   | {
-//       chartType: "bar";
-//       data: SampleBarChartData;
-//       option: SampleBarChartOptions;
-//     }
-//   | {
-//       chartType: "pie";
-//       data: SamplePieChartData;
-//       option: SamplePieChartOption;
-//     }
-//   | {
-//       chartType: "line";
-//       data: SampleLineChartData;
-//       option: SampleLineChartOption;
-//     };
+export type RenderDataType =
+  | {
+      chartType: "bar";
+      data: SampleBarChartData;
+      option: SampleBarChartOptions;
+    }
+  | {
+      chartType: "pie";
+      data: SamplePieChartData;
+      option: SamplePieChartOption;
+    }
+  | {
+      chartType: "line";
+      data: SampleLineChartData;
+      option: SampleLineChartOption;
+    };
+
+export type ShareDataType =
+  | {
+      chartType: "bar";
+      data: SampleBarChartData;
+      option: SampleBarChartOptions;
+      updatedAt: Timestamp;
+      createdAt: Timestamp;
+    }
+  | {
+      chartType: "pie";
+      data: SamplePieChartData;
+      option: SamplePieChartOption;
+      updatedAt: Timestamp;
+      createdAt: Timestamp;
+    }
+  | {
+      chartType: "line";
+      data: SampleLineChartData;
+      option: SampleLineChartOption;
+      updatedAt: Timestamp;
+      createdAt: Timestamp;
+    };
+
+export type StripDataType =
+  | {
+      chartType: "bar";
+      data: SampleBarChartData;
+      option: SampleBarChartOptions;
+      createdAt: Timestamp;
+    }
+  | {
+      chartType: "pie";
+      data: SamplePieChartData;
+      option: SamplePieChartOption;
+      createdAt: Timestamp;
+    }
+  | {
+      chartType: "line";
+      data: SampleLineChartData;
+      option: SampleLineChartOption;
+      createdAt: Timestamp;
+    };
