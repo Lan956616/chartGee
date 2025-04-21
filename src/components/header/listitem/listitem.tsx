aa5d8eb0e0049779866e29d7397b83fb4dbefcbc
Lan956616 <cd25924218@gmail.com>
2025-04-21 13:08:35 +0800

    feat(ListItem): support href or onClick rendering and adjust icon hover style

--- src/components/header/listitem/listitem.tsx
+++ src/components/header/listitem/listitem.tsx
@@ -1,33 +1,73 @@
 import Image from "next/image";
 import styles from "./listitem.module.css";
-type ListItemProps = {
-  children: React.ReactNode;
-  side: "right" | "down";
-  onClick?: () => void;
-};
-const ListItem: React.FC<ListItemProps> = ({ children, side, onClick }) => {
-  return (
-    <li className={styles.listItem} onClick={onClick}>
+import Link from "next/link";
+type ListItemProps =
+  | {
+      children: React.ReactNode;
+      side: "right" | "down";
+      onClick: () => void;
+      href?: never;
+    }
+  | {
+      children: React.ReactNode;
+      side: "right" | "down";
+      href: string;
+      onClick?: never;
+    };
+const ListItem: React.FC<ListItemProps> = ({
+  children,
+  side,
+  onClick,
+  href,
+}) => {
+  const content = (
+    <>
       <p>{children}</p>
       {side === "right" && (
-        <Image
-          src="/right-arrow.png"
-          alt="right-arrow"
-          width={15}
-          height={15}
-          className={styles.listItemImg}
-        />
+        <div className={styles.imageWrapper}>
+          <Image
+            src="/right-arrow.png"
+            alt="right-arrow"
+            fill
+            className={styles.listItemImg}
+          />
+          <Image
+            src="/blue-right-arrow.png"
+            alt="right-arrow"
+            fill
+            className={styles.hoverImg}
+          />
+        </div>
       )}
       {side === "down" && (
-        <Image
-          src="/down-arrow.png"
-          alt="down-arrow"
-          width={15}
-          height={15}
-          className={styles.listItemImg}
-        />
+        <div className={styles.imageWrapper}>
+          <Image
+            src="/down-arrow.png"
+            alt="down-arrow"
+            fill
+            className={styles.listItemImg}
+          />
+          <Image
+            src="/blue-down-arrow.png"
+            alt="down-arrow"
+            fill
+            className={styles.hoverImg}
+          />
+        </div>
       )}
-    </li>
+    </>
+  );
+  if (onClick) {
+    return (
+      <li className={styles.listItem} onClick={onClick}>
+        {content}
+      </li>
+    );
+  }
+  return (
+    <Link href={href} className={styles.listItem}>
+      {content}
+    </Link>
   );
 };
 
