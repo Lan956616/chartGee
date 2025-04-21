46b0b8262b63b4f109884775b328a31537064ade
Lan956616 <cd25924218@gmail.com>
2025-04-21 10:17:13 +0800

    feat: add uploadToFirestore function with isPublic support

---
+++ src/utils/uploadToFirestore.ts
@@ -0,0 +1,24 @@
+import type {
+  ProjectDataType,
+  StripDataType,
+} from "./sampleChartData/projectDataType";
+import { db } from "./firebase";
+import { doc, updateDoc, serverTimestamp } from "firebase/firestore";
+export const uploadToFirestore = async (
+  originalData: ProjectDataType,
+  currentData: StripDataType,
+  projectID: string,
+  uid: string
+) => {
+  const projectRef = doc(db, "users", uid, "projects", projectID);
+  await updateDoc(projectRef, {
+    ...currentData,
+    updatedAt: serverTimestamp(),
+  });
+  if (originalData.isPublic) {
+    const publicProjectRef = doc(db, "publicProjects", projectID);
+    await updateDoc(publicProjectRef, {
+      ...currentData,
+    });
+  }
+};
