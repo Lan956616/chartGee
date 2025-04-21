a81a8b0b5a2cc61a260db2ee74be9c085db82c04
Lan956616 <cd25924218@gmail.com>
2025-04-21 13:11:25 +0800

    feat(hamburger):move navbarSmall into hamburger component

--- src/components/header/hamburger/hamburger.tsx
+++ src/components/header/hamburger/hamburger.tsx
@@ -1,24 +1,28 @@
 import styles from "./hamburger.module.css";
 import Image from "next/image";
+import NavbarSmall from "../navbarsmall/navbarsmall";
 type HamburgerProps = {
   isClicked: boolean;
   setIsClicked: React.Dispatch<React.SetStateAction<boolean>>;
 };
 const Hamburger: React.FC<HamburgerProps> = ({ isClicked, setIsClicked }) => {
   return (
-    <div
-      className={styles.hamburgerWrapper}
-      onClick={() => {
-        setIsClicked(!isClicked);
-      }}
-    >
-      {isClicked && (
-        <Image src="/cross.png" alt="cross" width={20} height={20} />
-      )}
-      {!isClicked && (
-        <Image src="/hamburger.png" alt="cross" width={20} height={20} />
-      )}
-    </div>
+    <>
+      <div
+        className={styles.hamburgerWrapper}
+        onClick={() => {
+          setIsClicked(!isClicked);
+        }}
+      >
+        {isClicked && (
+          <Image src="/cross.png" alt="cross" width={20} height={20} />
+        )}
+        {!isClicked && (
+          <Image src="/hamburger.png" alt="cross" width={20} height={20} />
+        )}
+      </div>
+      <NavbarSmall isClicked={isClicked} />
+    </>
   );
 };
 
