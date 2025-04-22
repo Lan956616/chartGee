151d859688836500546192ec34e4fbf4bf531960
Lan956616 <cd25924218@gmail.com>
2025-04-22 12:48:24 +0800

    feat(shareButton): remove download logic and add props to control sharePopUp component

--- src/components/headereditpage/sharebutton/sharebutton.tsx
+++ src/components/headereditpage/sharebutton/sharebutton.tsx
@@ -1,48 +1,26 @@
 import styles from "./sharebutton.module.css";
 import Image from "next/image";
-import { useState, useRef } from "react";
-import { useClickWheelOutside } from "@/hooks/useClickWheelOutside";
-import ListItem from "../smallsidebar/listitem/listitem";
-const ShareButton: React.FC = () => {
-  const [isClicked, setIsClicked] = useState<boolean>(false);
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
-  const popUpRef = useRef<HTMLDivElement | null>(null);
-  useClickWheelOutside(popUpRef, buttonRef, isClicked, () => {
-    setIsClicked(false);
-  });
+import type { Dispatch, SetStateAction } from "react";
+type ShareButtonProps = {
+  setShowSharePopUp: Dispatch<SetStateAction<boolean>>;
+};
+const ShareButton: React.FC<ShareButtonProps> = ({ setShowSharePopUp }) => {
   return (
-    <div className={styles.ShareButtonContainer}>
-      <button
-        className={`${styles.btn} ${isClicked && styles.clicked}`}
-        onClick={() => {
-          setIsClicked((prev) => {
-            return !prev;
-          });
-        }}
-        ref={buttonRef}
-      >
-        <Image
-          src="/share.png"
-          alt="share-icon"
-          width={25}
-          height={25}
-          className={styles.btnImg}
-        />
-        Share
-      </button>
-      <div
-        className={styles.sharePopUp}
-        style={{ display: `${isClicked ? "" : "none"}` }}
-        ref={popUpRef}
-      >
-        <ListItem href="/" src="/download.png" alt="download-icon">
-          Download
-        </ListItem>
-        <ListItem href="/" src="/link.png" alt="link-icon">
-          Share Link
-        </ListItem>
-      </div>
-    </div>
+    <button
+      className={styles.btn}
+      onClick={() => {
+        setShowSharePopUp(true);
+      }}
+    >
+      <Image
+        src="/share.png"
+        alt="share-icon"
+        width={20}
+        height={20}
+        className={styles.btnImg}
+      />
+      <p className={styles.title}>Share</p>
+    </button>
   );
 };
 
