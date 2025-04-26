5fede1486d2a8376736835317e21b40e6d49fa09
Lan956616 <cd25924218@gmail.com>
2025-04-26 12:40:16 +0800

    feat(handleInputKeyDown): prevent form submit when press enter

--- src/utils/handleInputKeyDown.ts
+++ src/utils/handleInputKeyDown.ts
@@ -2,6 +2,7 @@ export const handleInputKeyDown = (
   e: React.KeyboardEvent<HTMLInputElement>
 ) => {
   if (e.key === "Enter" || e.key === "Escape") {
+    e.preventDefault();
     e.currentTarget.blur();
   }
 };
