b9889ff1c5c7b73170112b2b435d608b7b3fba8e
Lan956616 <cd25924218@gmail.com>
2025-05-12 17:14:38 +0800

    refactor(TableControlButton): replace Button component with native button element

--- src/components/dataarea/datatab/tablecontrolbutton/tablecontrolbutton.tsx
+++ src/components/dataarea/datatab/tablecontrolbutton/tablecontrolbutton.tsx
@@ -1,30 +1,26 @@
 "use client";
 import styles from "./tablecontrolbutton.module.css";
-import Button from "@/components/button/button";
 import { useContext } from "react";
 import { ChartDataContext } from "@/components/ChartDataProvider";
 import { resetDataOnClick, clearDataOnClick } from "@/utils/controlButtonFunc";
-
 const TableControlButton: React.FC = () => {
   const context = useContext(ChartDataContext);
   if (!context) return;
   const { setCurrentData } = context;
   return (
     <div className={styles.tableControlButton}>
-      <Button
+      <button
         className={styles.btn}
-        width={50}
         onClick={() => clearDataOnClick(setCurrentData)}
       >
         Clear Data
-      </Button>
-      <Button
+      </button>
+      <button
         className={styles.btn}
-        width={50}
         onClick={() => resetDataOnClick(setCurrentData)}
       >
         Reset Data
-      </Button>
+      </button>
     </div>
   );
 };
